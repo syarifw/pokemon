f0d270623b7b27aae15ce9714c18e492dbad8008
Syarif Waliyuddin <syarifwaliyuddin@gmail.com>
2022-12-07 00:40:28 +0700

    Add script migrate to mysql, config mysql, & queries: create and inject tables ability & basic_data, temporary table for core data, grouping based on sum of ability

---
+++ migrate_to_mysql.py
@@ -0,0 +1,55 @@
+from mysql.connector import connect,Error
+from config import *
+from query import *
+
+import requests
+
+url_get_pokemon_list = "https://pokeapi.co/api/v2/pokemon/?limit=2000&offset=0"
+
+response_pokemon_list = requests.get(url_get_pokemon_list).json()
+# n=0
+list_pokemon = response_pokemon_list["results"]
+for index in list_pokemon:
+    # if n <= 0:
+    get_detail_pokemon = requests.get(index["url"]).json()
+
+    #Inject to MySQL
+    try:
+        mydb = connect(
+            host=host,
+            user=username,
+            password=password,
+            database=database
+        )
+        serial = get_detail_pokemon["name"]+str(get_detail_pokemon["id"])
+        mycursor = mydb.cursor()
+
+        # Inject to basic_data
+        mycursor.execute(query_inject_basic_data,{
+            'name':get_detail_pokemon["name"],
+            'serial':serial,
+            'species':get_detail_pokemon["species"]["name"],
+            'weight':get_detail_pokemon["weight"],
+            'height':get_detail_pokemon["height"]})
+        ability_order = 1
+        for index_1 in get_detail_pokemon["abilities"]:
+            #Inject to ability
+            mycursor.execute(query_inject_ability,{
+            'name':get_detail_pokemon["name"],
+            'serial':serial,
+            'ability':index_1["ability"]["name"],
+            'slot':index_1['slot'],
+            'ability_order':ability_order})
+            ability_order+=1
+        # myresult = mycursor.fetchall()
+        # print(myresult)
+        print(f"{serial} with name: already injected")
+    except Error as e:
+        print(f"Failed to inject data: {e}")
+    finally:
+        mydb.commit()
+        if mydb.is_connected():
+            mydb.close()
+        # n+=1
+print("MySQL Connection is closed.")
+
---
+++ query.py
@@ -0,0 +1,83 @@
+query_create_basic_data = """
+Create table basic_data_1
+(
+	id		INT NOT NULL auto_increment,
+    serial VARCHAR(100) not null,
+    name	VARCHAR(100),
+    species VARCHAR(100),
+    weight INT unsigned not null default 0,
+    height INT unsigned not null default 0,
+    primary key (id),
+    unique key serial_unique (serial),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp
+) ENGINE = InnoDB;
+"""
+
+query_create_ability = """
+create table ability_1
+(
+	id		INT NOT NULL auto_increment,
+    serial	VARCHAR(100) not null,
+    name	VARCHAR(100),
+    ability VARCHAR(100) not null,
+    slot INT,
+    ability_order INT,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp,
+    primary key (id)
+) ENGINE = InnoDB;
+"""
+
+query_inject_basic_data = """
+    INSERT INTO basic_data_1 (name,serial,species,weight,height)
+    values (%(name)s,%(serial)s,%(species)s,%(weight)s,%(height)s)
+"""
+
+query_inject_ability = """
+    INSERT INTO ability_1 (name,serial,ability,slot,ability_order)
+    values (%(name)s,%(serial)s,%(ability)s,%(slot)s,%(ability_order)s)
+"""
+
+query_temp_table_pokedex = """
+Select 
+	bd.name, 
+    bd.serial, 
+    skill_1.ability as ability_1, 
+    skill_2.ability as ability_2, 
+    skill_3.ability as ability_3,
+    bd.species,
+    bd.weight,
+    bd.height
+from basic_data_1 bd 
+left join 
+	ability_1 skill_1 
+    on bd.serial = skill_1.serial 
+    and skill_1.ability_order = 1 
+left join 
+	ability_1 skill_2 
+    on bd.serial = skill_2.serial 
+    and skill_2.ability_order = 2 
+left join 
+	ability_1 skill_3 
+	on bd.serial = skill_3.serial 
+    and skill_3.ability_order = 3 
+"""
+
+query_ability_1 = f"""
+with temp_1 as 
+    ({query_temp_table_pokedex})
+select * from temp_1 where ability_1 is not null and ability_2 is null;
+"""
+
+query_ability_2 = f"""
+with temp_1 as 
+    ({query_temp_table_pokedex})
+select * from temp_1 where ability_2 is not null and ability_3 is null;
+"""
+
+query_ability_3 = f"""
+with temp_1 as 
+    ({query_temp_table_pokedex})
+select * from temp_1 where ability_3 is not null;
+"""
